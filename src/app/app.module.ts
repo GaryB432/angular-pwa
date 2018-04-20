645c6ea9ec2b7f331fc48eb11ee7ecdddf227e4b
Gary Bortosky <garybort@gmail.com>
2018-04-20 09:57:41 -0500

    add app with fa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
