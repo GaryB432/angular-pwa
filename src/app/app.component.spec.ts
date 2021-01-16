1db07c4ee77699a6e0ae260ed371916072af056f
Gary Bortosky <garybort@gmail.com>
2021-01-15 21:27:55 -0600

    reinstate

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to gbg!');
+    expect(compiled.querySelector('h1').textContent).toContain('gbg');
   }));
 });
